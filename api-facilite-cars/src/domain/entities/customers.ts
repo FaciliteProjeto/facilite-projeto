009f88a6a2a8ff5ddd7905cb3efac8b278aad402
marlisonmourao <bentesmourao@gmail.com>
2024-10-19 22:27:08 -0400

    feat: create entities cars and customers

--- api-facilite-cars/src/domain/entities/customers.ts
+++ api-facilite-cars/src/domain/entities/customers.ts
@@ -1,18 +1,34 @@
-import { randomUUID } from 'node:crypto'
+import { Entity } from '@/core/entities/entity'
+import type { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import type { Optional } from '@/core/types/optional'
 
 interface CustomersProps {
   name: string
   cpf: string
+  createdAt: Date
 }
 
-export class Customers {
-  public id: string
-  public name: string
-  public cpf: string
+export class Customers extends Entity<CustomersProps> {
+  get name() {
+    return this.props.name
+  }
 
-  constructor(props: CustomersProps, id?: string) {
-    this.id = id ?? randomUUID()
-    this.name = props.name
-    this.cpf = props.cpf
+  get cpf() {
+    return this.props.cpf
+  }
+
+  static create(
+    props: Optional<CustomersProps, 'createdAt'>,
+    id?: UniqueEntityID
+  ) {
+    const customers = new Customers(
+      {
+        ...props,
+        createdAt: new Date(),
+      },
+      id
+    )
+
+    return customers
   }
 }
--- api-facilite-cars/src/domain/entities/order.ts
+++ api-facilite-cars/src/domain/entities/order.ts
@@ -1,18 +1,32 @@
-import { randomUUID } from 'node:crypto'
+import { Entity } from '@/core/entities/entity'
+import type { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import type { Optional } from '@/core/types/optional'
 
 interface OrderProps {
-  userId: string
-  customerId
+  userId: UniqueEntityID
+  customerId: UniqueEntityID
+  createdAt: Date
+  updatedAt?: Date
 }
 
-export class Order {
-  public id: string
-  public userId: string
-  public customerId: string
+export class Order extends Entity<OrderProps> {
+  get userId() {
+    return this.props.userId
+  }
 
-  constructor(props: OrderProps, id?: string) {
-    this.userId = props.userId
-    this.customerId = props.customerId
-    this.id = id ?? randomUUID()
+  get customerId() {
+    return this.props.customerId
+  }
+
+  static create(props: Optional<OrderProps, 'createdAt'>, id?: UniqueEntityID) {
+    const order = new Order(
+      {
+        ...props,
+        createdAt: new Date(),
+      },
+      id
+    )
+
+    return order
   }
 }
