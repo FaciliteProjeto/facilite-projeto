5b67707aca2be3c1c88505c5d8fc8fcf489dde98
marlisonmourao <bentesmourao@gmail.com>
2024-11-15 18:12:18 -0400

    feat: create layout home page

--- facilite-web/src/app/(app)/layout.tsx
+++ facilite-web/src/app/(app)/layout.tsx
@@ -1,4 +1,9 @@
 import { isAuthenticated } from '@/auth/auth'
+import { AppSidebar } from '@/components/app-sidebar'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Input } from '@/components/ui/input'
+import { SidebarProvider } from '@/components/ui/sidebar'
+import { ArrowBigDown, Bell, Search } from 'lucide-react'
 import { redirect } from 'next/navigation'
 
 export default async function AppLayout({
@@ -12,5 +17,40 @@ export default async function AppLayout({
     redirect('/auth/sign-in')
   }
 
-  return <>{children}</>
+  return (
+    <>
+      <SidebarProvider>
+        <AppSidebar />
+
+        <div className="w-full">
+          <div className="bg-yellow-400 w-full p-2 items-center justify-between flex flex-row">
+            <h2 className="font-medium text-black">
+              Seja bem vindo Marlison Bentes Mourão
+            </h2>
+
+            <div>
+              <div className="flex gap-2 items-center justify-center bg-white rounded-2xl">
+                <Search size={20} className="text-gray-950 pl-2" />
+                <Input className="border-none ring-0 focus:ring-0 outline-none focus:outline-none focus-visible:ring-0 focus-visible:ring-offset-0 flex-1" />
+              </div>
+            </div>
+
+            <div className="flex items-center gap-3">
+              <Bell size={20} />
+
+              <Avatar className="size-7">
+                <AvatarFallback />
+
+                <AvatarImage src="https://github.com/marlisonmourao.png" />
+              </Avatar>
+
+              <ArrowBigDown size={25} />
+            </div>
+          </div>
+
+          {children}
+        </div>
+      </SidebarProvider>
+    </>
+  )
 }
---
+++ facilite-web/src/components/app-sidebar.tsx
@@ -0,0 +1,78 @@
+'use client'
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarTrigger,
+} from '@/components/ui/sidebar'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+const items = [
+  {
+    title: 'Catálogo',
+    url: '/',
+  },
+  {
+    title: 'Pagamentos',
+    url: '/pagamentos',
+  },
+]
+
+export function AppSidebar() {
+  const pathname = usePathname()
+
+  return (
+    <Sidebar>
+      <SidebarContent className="bg-black">
+        <div className="w-full flex justify-end p-2">
+          <SidebarTrigger className="text-white" />
+        </div>
+        <SidebarHeader className="w-full items-center justify-center mt-5">
+          <Image
+            src="/logo-facilite.svg"
+            alt="facilite"
+            width={0}
+            height={0}
+            className="w-28 h-[100px]"
+            sizes="100vh"
+            style={{
+              objectFit: 'contain',
+            }}
+          />
+        </SidebarHeader>
+
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu className="space-y-3">
+              {items.map(item => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link
+                      href={item.url}
+                      className={`flex items-center justify-center text-sm font-medium w-full h-full p-2 
+                        ${
+                          pathname === item.url
+                            ? 'bg-yellow-400 text-black'
+                            : 'text-white hover:bg-yellow-400 hover:text-black focus:bg-yellow-400 focus:text-black'
+                        }`}
+                    >
+                      {item.title}
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}
